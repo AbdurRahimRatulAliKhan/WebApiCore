82c18f64da98d8d3005181df650ba1caec1e04e4
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-27 18:57:40 +0600

    Add project files.

---
+++ WebApiCore/Model/EmployeeContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCore.Model
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+        {
+
+        }
+    }
+}
